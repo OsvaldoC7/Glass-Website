cf8a78a6cbdcea98b13290761162a24ea80f650a
OsvaldoC7 <osva.6cruz@gmail.com>
2021-10-17 22:36:43 -0500

    Adding sort funcionality

--- main.js
+++ main.js
@@ -5,8 +5,18 @@ Sortable.create(lista, {
     chosenClass: 'seleccionado',
     // ghostClass: 'fantasma',
     // dragClass: 'drag',
-    // onEnd: () => {
-
-    // },
-    group: 'cards'
+    onEnd: () => {
+        console.log("Se inserto un elemento")
+    },
+    group: 'cards',
+    store: {
+        set: (sortable) => {
+            const orden = sortable.toArray();
+            localStorage.setItem(sortable.options.group.name, orden.join('|'));
+        },
+        get: (sortable) => {
+            const orden = localStorage.getItem(sortable.options.group.name);
+            return orden ? orden.split('|') : [];
+        }
+    }
 });
